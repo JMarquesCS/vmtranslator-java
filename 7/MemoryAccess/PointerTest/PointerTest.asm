a2dcff5ec83e75333e12b96516d2f7e4284d7c9c
Emanuelle <emanuellefernandes75@gmail.com>
2025-02-12 00:50:17 -0300

    teste pointerTest

---
+++ 7/MemoryAccess/PointerTest/PointerTest.asm
@@ -0,0 +1,172 @@
+@256
+D=A
+@SP
+M=D
+@Sys.init_RETURN_0 // call Sys.init 0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@LCL
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@ARG
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THIS
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THAT
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@0
+D=A
+@5
+D=D+A
+@SP
+D=M-D
+@ARG
+M=D
+@SP
+D=M
+@LCL
+M=D
+@Sys.init
+0;JMP
+(Sys.init_RETURN_0)
+@3030 // push constant 3030
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // pop pointer 0
+M=M-1
+A=M
+D=M
+@R3
+M=D
+@3040 // push constant 3040
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // pop pointer 1
+M=M-1
+A=M
+D=M
+@R4
+M=D
+@32 // push constant 32
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THIS // pop this 2
+D=M
+@2
+D=D+A
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+A=M
+M=D
+@46 // push constant 46
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@THAT // pop that 6
+D=M
+@6
+D=D+A
+@R13
+M=D
+@SP
+M=M-1
+A=M
+D=M
+@R13
+A=M
+M=D
+@R3 // push pointer 0
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@R4 // push pointer 1
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // add
+M=M-1
+A=M
+D=M
+A=A-1
+M=D+M
+@THIS // push this 2
+D=M
+@2
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // sub
+M=M-1
+A=M
+D=M
+A=A-1
+M=M-D
+@THAT // push that 6
+D=M
+@6
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP // add
+M=M-1
+A=M
+D=M
+A=A-1
+M=D+M
